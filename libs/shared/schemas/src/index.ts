c9fb99812ab0ca97d542c3b649ed5758a0a76e72
getlarge <ed@getlarge.eu>
2024-01-25 16:34:59 +0100

    feat(schemas): generate exportables

--- libs/shared/schemas/src/index.ts
+++ libs/shared/schemas/src/index.ts
@@ -1,1 +1,10 @@
-export * from './lib/schemas';
+import batch from './lib/batch.json';
+import lot from './lib/lot.json';
+import reception from './lib/reception.json';
+
+export { batch, lot, reception };
+export default {
+  batch,
+  lot,
+  reception,
+};
