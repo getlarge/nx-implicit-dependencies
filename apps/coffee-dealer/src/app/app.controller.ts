491b6b49d9f7aed55e7bc8daf00ca5670ddf3ace
getlarge <ed@getlarge.eu>
2024-01-26 14:07:10 +0100

    refactor(coffee-dealer): replace NestJS controller decorators by Nestia

--- apps/coffee-dealer/src/app/app.controller.ts
+++ apps/coffee-dealer/src/app/app.controller.ts
@@ -1,32 +1,30 @@
 import { Batch, Lot, Reception } from '@getlarge/ts-interfaces';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TypedBody, TypedRoute } from '@nestia/core';
+import { Controller } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { IsValidBatchPipe } from './pipes/batch.pipe';
-import { IsValidReceptionPipe } from './pipes/reception.pipe';
-import { IsValidLotPipe } from './pipes/lot.pipe';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
+  @TypedRoute.Get()
+  getData(): { message: string } {
     return this.appService.getData();
   }
 
-  @Post('batch')
-  batch(@Body(IsValidBatchPipe) body: Batch): Promise<Batch> {
+  @TypedRoute.Post('batch')
+  batch(@TypedBody() body: Batch): Promise<Batch> {
     return this.appService.createBatch(body);
   }
 
-  @Post('reception')
-  reception(@Body(IsValidReceptionPipe) body: Reception): Promise<Reception> {
+  @TypedRoute.Post('reception')
+  reception(@TypedBody() body: Reception): Promise<Reception> {
     return this.appService.createReception(body);
   }
 
-  @Post('lot')
-  lot(@Body(IsValidLotPipe) body: Lot): Promise<Lot> {
+  @TypedRoute.Post('lot')
+  lot(@TypedBody() body: Lot): Promise<Lot> {
     return this.appService.createLot(body);
   }
 }
